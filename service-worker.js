1bd1a682ddf07fee797e38e39a1f281416c82e3d
a.khotianovich <a.khotianovich@sdventures.com>
2017-02-20 15:53:13 +0300

    service worker fetch demo

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,28 @@
+self.addEventListener('fetch', function(event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+            console.log('No response found in cache. About to fetch from network...');
+
+            return fetch(event.request).then(function(response) {
+                console.log('Response from network is:', response);
+
+                return response;
+            }).catch(function(error) {
+                console.error('Fetching failed:', error);
+
+                throw error;
+            });
+        })
+    );
+});
+/*
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     '/main.css',
@@ -72,4 +97,4 @@ self.addEventListener('activate', function(event) {
             );
         })
     );
-});
+});*/
