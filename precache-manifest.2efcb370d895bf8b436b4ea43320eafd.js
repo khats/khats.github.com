39231a72645566803f97a67481923a38d1228221
khatsian <micropasa@gmail.com>
2019-10-15 00:53:26 +0300

    minesweeper

---
+++ precache-manifest.2efcb370d895bf8b436b4ea43320eafd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b627ca525a36e1ec61dc743c753d3bc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "758ba22abb2451df2711",
+    "url": "/static/css/main.1719db70.chunk.css"
+  },
+  {
+    "revision": "24bacb460ea58f5511dc",
+    "url": "/static/js/2.f5159e33.chunk.js"
+  },
+  {
+    "revision": "758ba22abb2451df2711",
+    "url": "/static/js/main.45a64759.chunk.js"
+  },
+  {
+    "revision": "c566206f857691115840",
+    "url": "/static/js/runtime-main.705b19b6.js"
+  }
+]);
